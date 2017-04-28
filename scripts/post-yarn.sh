a01d06c3b8cc75cb7b910d948d17befbef62518c
deeforte <deirdre.forte@libertymutual.com>
2017-04-28 09:15:02 -0400

    Commit for stash api before deploying to heroku
    Got curl requests working locally now deploying to heroku

--- scripts/post-yarn.sh
+++ scripts/post-yarn.sh
@@ -5,15 +5,15 @@ URL_PATH="/sign-up"
 TOKEN="BAhJIiVlMDhhY2I0MzM1OGYyOTg1NGJjYjg0Zjg2ZjViYTI3OQY6BkVG--b685b613fc2cd064231ea1a080eff1f6f105cf41"
 curl --include --request POST http://localhost:4741/yarns \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=BAhJIiVlMDhhY2I0MzM1OGYyOTg1NGJjYjg0Zjg2ZjViYTI3OQY6BkVG--b685b613fc2cd064231ea1a080eff1f6f105cf41" \
+  --header "Authorization: Token token=BAhJIiUxOTdkZWY5Y2ZiN2NlZmM0OTgzNzBlY2M2MTk1ODJhMgY6BkVG--4c9365cfa4fbc086c4f639ba1c6fe17bbeb4a696" \
   --data '{
     "yarn": {
       "user_id": 2,
-      "name": "mountain mohair",
+      "name": "shetlan",
       "quantity":3,
       "yards":250,
       "color": "gray",
-      "project": "sweater"
+      "project": "shawl"
     }
   }'
 
